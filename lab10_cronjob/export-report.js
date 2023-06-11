faecad6bac34ad56f99a9695560126a45333250d
K.Banyawat <banyawat@igeargeek.com>
2023-06-11 22:25:21 +0700

    feat: cronjob lab init

---
+++ lab10_cronjob/export-report.js
@@ -0,0 +1,6 @@
+console.log(`${new Date()} Exporting report from database...`)
+
+setTimeout(() => {
+  console.log(`${new Date()} Export success`)
+  process.exit(0)
+}, 5000)
---
+++ lab9_job/migrate.js
@@ -0,0 +1,6 @@
+console.log(`${new Date()} Start migration...`)
+
+setTimeout(() => {
+  console.log(`${new Date()} Migration success`)
+  process.exit(0)
+}, 5000)
